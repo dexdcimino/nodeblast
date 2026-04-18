1371faab432c8e7b3de1a5ebccb16c4912c549e1
Dex <dexdcimino@gmail.com>
2026-04-18 05:06:06 -0600

    MD4: slow hex hover + neighbor push effect

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nodeblast-v126';
+const CACHE_NAME = 'nodeblast-v127';
 const STATIC_ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', (e) => {
