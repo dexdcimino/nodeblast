1facf1a8ee456a9ffba9882af25889f2ecca47bb
Dex <dexdcimino@gmail.com>
2026-04-18 00:26:33 -0600

    MD67: kill scroll bounce on alchemist cards

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nodeblast-v120';
+const CACHE_NAME = 'nodeblast-v121';
 const STATIC_ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', (e) => {
