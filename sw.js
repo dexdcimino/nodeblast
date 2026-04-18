c3ff7301cd28ed4b383657113e0c43f3836690c5
Dex <dexdcimino@gmail.com>
2026-04-18 04:13:45 -0600

    MD3: pinned alchemist expanded gets more space

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nodeblast-v123';
+const CACHE_NAME = 'nodeblast-v124';
 const STATIC_ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', (e) => {
