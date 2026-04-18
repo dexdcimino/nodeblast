587bc49edd089112be78d330a12a1cae67ccdc89
Dex <dexdcimino@gmail.com>
2026-04-18 05:03:50 -0600

    MD3: pinned alchemist stays 50%, tiles shrink to fit

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nodeblast-v125';
+const CACHE_NAME = 'nodeblast-v126';
 const STATIC_ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', (e) => {
