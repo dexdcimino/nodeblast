fd17d0bd6c341948256fba5b785d682bece029ec
Dex <dexdcimino@gmail.com>
2026-04-18 18:58:32 -0600

    MD1: kill scroll bounce + remove neighbor hover from cards

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nodeblast-v130';
+const CACHE_NAME = 'nodeblast-v131';
 const STATIC_ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', (e) => {
