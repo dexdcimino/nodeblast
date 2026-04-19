194563b5fa0f7b9906ea680695d62225860aacda
Dex <dexdcimino@gmail.com>
2026-04-18 19:42:10 -0600

    MD#2: Minimap arrow points up

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nodeblast-v135';
+const CACHE_NAME = 'nodeblast-v136';
 const STATIC_ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', (e) => {
