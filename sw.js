3f9d8faf276f57d5c83399eccc5fe23129f9e709
Dex <dexdcimino@gmail.com>
2026-04-18 19:34:37 -0600

    MD2: vote pill circle + pin/share/collapse circles

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nodeblast-v131';
+const CACHE_NAME = 'nodeblast-v132';
 const STATIC_ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', (e) => {
